e18a38bfc853054ced1496db36ed1639a8490cf8
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-06 01:56:45 +0300

    Initial Commit

---
+++ FavoriteAppUI/FavoriteAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteAppUIApp.swift
+//  FavoriteAppUI
+//
+//  Created by Ömer on 6.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FavoriteAppUITests/FavoriteAppUITests.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteAppUITests.swift
+//  FavoriteAppUITests
+//
+//  Created by Ömer on 6.09.2025.
+//
+
+import Testing
+@testable import FavoriteAppUI
+
+struct FavoriteAppUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
