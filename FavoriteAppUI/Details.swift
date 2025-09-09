09c095b57e810095d1e86bef8542193285efcba9
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-10 01:23:47 +0300

    Details

---
+++ FavoriteAppUI/Details.swift
@@ -0,0 +1,18 @@
+//
+//  Details.swift
+//  FavoriteAppUI
+//
+//  Created by Ömer on 10.09.2025.
+//
+
+import SwiftUI
+
+struct Details: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Details()
+}
